86100cbd1bfaa661002878fa731f1b40faede57a
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-03-08 16:09:38 +0100

    gh pages setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const debug = process.env.NODE_ENV !== 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: !debug ? '/visap2022/' : '',
 }
 
 module.exports = nextConfig
