4c3c18ee0c652b3fcb2ceebb0ba73dfecbd9e0a9
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-03-14 15:33:12 +0100

    added pages

---
+++ pages/call-for-entries.js
@@ -0,0 +1,9 @@
+import PageTemplate from "../components/PageTemplate/PageTemplate";
+
+export default function Submission() {
+  return (
+    <PageTemplate>
+      call-for-entries
+    </PageTemplate>
+  )
+}
---
+++ pages/submission.js
@@ -0,0 +1,9 @@
+import PageTemplate from "../components/PageTemplate/PageTemplate";
+
+export default function Submission() {
+  return (
+    <PageTemplate>
+      Submission page
+    </PageTemplate>
+  )
+}
