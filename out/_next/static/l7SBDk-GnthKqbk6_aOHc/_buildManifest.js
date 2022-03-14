a13ad0147ad0b30a526aa6768f2f64bcc39628da
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-03-14 15:33:40 +0100

    Deploy Next.js to gh-pages

--- out/_next/static/l7SBDk-GnthKqbk6_aOHc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/31d2c10d0f7679fa.css","static/chunks/pages/index-c4d4b556aa970bb8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
