b5e7121dbbf107a000d08a32e584a535fcde1f65
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-09-27 13:37:33 +0200

    image (quasi) optimization in single contribution

--- pages/contributions.js
+++ pages/contributions.js
@@ -1,5 +1,10 @@
-import PageTemplate from "../components/PageTemplate/PageTemplate";
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import stylesPageTemplate from "../components/PageTemplate/PageTemplate.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
 import classNames from "classnames";
+
 import { getContributionsData } from "../lib/contributions";
 import Link from "next/link";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
@@ -7,7 +12,7 @@ import styles from "../styles/contributions.module.scss";
 import { useRouter } from "next/router";
 import ExportedImage from "next-image-export-optimizer";
 
-export default function Contributions({ allContributionsData }) {
+export default function Contributions({ allContributionsData, metaContent }) {
 	const { basePath } = useRouter();
 	const masonryBreakpoints = {
 		default: 4,
@@ -18,52 +23,87 @@ export default function Contributions({ allContributionsData }) {
 		576: 1,
 	};
 	return (
-		<PageTemplate metaTitle="Selected Contributions">
-			<h1 className={classNames("text-gradient", "page-title")}>Selected Contributions</h1>
-			<div id="contributions-list" className={classNames("XstickyTitleSection")}>
-				<h3 className={classNames("Xsticky-top")}>Artworks, Pictorials and Papers</h3>
-				{/* <p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</p> */}
-			</div>
-			<ResponsiveMasonry columnsCountBreakPoints={masonryBreakpoints}>
-				<Masonry gutter={"1.5rem"}>
-					{allContributionsData.map((d, i) => (
-						<Link key={"cont" + i} href={"/contributions/" + d.id} passHref>
-							<div
-								key={d.id}
-								dataname={d.id}
-								className={classNames(styles.contribution)}
-								style={{
-									borderColor: `var(${d.type === "artwork" ? "--pink" : "--azure"})`,
-									boxShadow: `7px 7px 2px 0px var(${d.type === "artwork" ? "--pink" : "--azure"})`,
-								}}
-							>
-								<div className={classNames(styles.thumbnail)}>
-									<ExportedImage
-										src={basePath + "/images/contributions-media/" + d.pc_id + "/" + d.images[0]}
-										alt={"Preview image of " + d.title}
-										layout="fill"
-										objectFit="cover"
-										placeholder="blur"
-									/>
-								</div>
-								<div className="px-2 pb-2">
-									<h6 className={classNames(styles.category, "mt-2", "mb-1")}>{d.type}</h6>
-									<h5 className={classNames(styles.title, "mb-2")}>{d.title}</h5>
-									<p className={classNames(styles.authors, "mb-1")}>{d.authors.map((d) => `${d.name} ${d.surname}`).join(", ")}</p>
-									<a className={classNames(styles.readMore, "m-0")}>Read more</a>
-								</div>
+		<>
+			<Head>
+				<title>{"Selected Contributions | VISAP'22"}</title>
+				<meta name="description" content={metaContent} />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Header />
+			<div className={classNames({ [stylesPageTemplate.windowFrame]: false })}>
+				<Container>
+					<Row>
+						<Col
+							sm={{ span: 12, offset: 0 }}
+							md={{ span: 10, offset: 1 }}
+							lg={{ span: 8, offset: 2 }}
+							xl={{ span: 8, offset: 2 }}
+							className={classNames(stylesPageTemplate.pageTitle, "mb-5")}
+						>
+							<h1 className={classNames("text-gradient", "page-title")}>Selected Contributions</h1>
+							<div id="contributions-list" className={classNames("XstickyTitleSection")}>
+								<h6 className={classNames("Xsticky-top")}><span style={{color: "var(--pink)"}}>Artworks</span>, <span style={{color: "var(--azure)"}}>Pictorials and Papers</span></h6>
+								{/* <p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+								</p> */}
 							</div>
-						</Link>
-					))}
-				</Masonry>
-			</ResponsiveMasonry>
-		</PageTemplate>
+						</Col>
+						<Col
+							sm={{ span: 12, offset: 0 }}
+							md={{ span: 10, offset: 1 }}
+							// lg={{ span: 8, offset: 2 }}
+							// xl={{ span: 8, offset: 2 }}
+							className={classNames(stylesPageTemplate.pageTitle, "mb-5")}
+						>
+							<ResponsiveMasonry columnsCountBreakPoints={masonryBreakpoints}>
+								<Masonry gutter={"1.5rem"}>
+									{allContributionsData.map((d, i) => (
+										<Link key={"cont" + i} href={"/contributions/" + d.id} passHref>
+											<div
+												key={d.id}
+												dataname={d.id}
+												className={classNames(styles.contribution)}
+												style={{
+													borderColor: `var(${d.type === "artwork" ? "--pink" : "--azure"})`,
+													boxShadow: `7px 7px 2px 0px var(${d.type === "artwork" ? "--pink" : "--azure"})`,
+												}}
+											>
+												<div className={classNames(styles.thumbnail)}>
+													<ExportedImage
+														src={basePath + "/images/contributions-media/" + d.pc_id + "/" + d.images[0]}
+														alt={"Preview image of " + d.title}
+														layout="fill"
+														objectFit="cover"
+														placeholder="blur"
+													/>
+												</div>
+												<div className="px-2 pb-2">
+													<h6 className={classNames(styles.category, "mt-2", "mb-1")}>{d.type}</h6>
+													<h5 className={classNames(styles.title, "mb-2")}>{d.title}</h5>
+													<p className={classNames(styles.authors, "mb-1")}>{d.authors.map((d) => `${d.name} ${d.surname}`).join(", ")}</p>
+													<a className={classNames(styles.readMore, "m-0")}>Read more</a>
+												</div>
+											</div>
+										</Link>
+									))}
+								</Masonry>
+							</ResponsiveMasonry>
+						</Col>
+					</Row>
+				</Container>
+				<Footer />
+			</div>
+		</>
 	);
 }
+
+Contributions.defaultProps = {
+	submenu: [],
+	metaContent:
+		"The VIS Arts Program (VISAP) is a mini-conference and exhibition where visualization researchers, designers, and artists come together to discuss works at the intersection of data visualization, art and design.",
+};
 
 export async function getStaticProps() {
 	const allContributionsData = getContributionsData();
--- pages/contributions/[id].js
+++ pages/contributions/[id].js
@@ -5,6 +5,9 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { getIds, getSingleData } from "../../lib/contributions";
 import { Row, Col } from "react-bootstrap";
 import { useRouter } from "next/router";
+import { useLayoutEffect, useState } from "react";
+import { image as d3Image } from "d3";
+import ExportedImage from "next-image-export-optimizer";
 
 export default function Contribution({ data }) {
 	const { basePath } = useRouter();
@@ -16,6 +19,38 @@ export default function Contribution({ data }) {
 		768: 2,
 		576: 1,
 	};
+	const [images, setImages] = useState([]);
+	const [authorsImages, setAuthorsImages] = useState([]);
+	useLayoutEffect(() => {
+		const arr = [];
+		data.images.forEach((d) => {
+			const imgSrc = basePath + "/images/contributions-media/" + data.pc_id + "/" + d;
+			arr.push(d3Image(imgSrc));
+		});
+		Promise.all(arr).then((arr) => {
+			const _images = [];
+			arr.forEach((img) => {
+				const { width, height, src } = img;
+				_images.push({ width, height, src });
+			});
+			setImages(_images);
+		});
+
+		const arr2 = [];
+		data.authors.forEach((d) => {
+			const imgSrc = basePath + "/images/contributions-media/" + data.pc_id + "/" + d.image;
+			arr2.push(d3Image(imgSrc));
+		});
+		Promise.all(arr2).then((arr2) => {
+			const _authorsImages = [];
+			arr2.forEach((img) => {
+				const { width, height, src } = img;
+				_authorsImages.push({ width, height, src });
+			});
+			setAuthorsImages(_authorsImages);
+		});
+	}, [data]);
+
 	return (
 		<ContributionPageTemplate metaTitle={data.title}>
 			<Col
@@ -34,14 +69,25 @@ export default function Contribution({ data }) {
 			<Col sm={{ span: 12, offset: 0 }} md={{ span: 10, offset: 1 }} className={classNames("mb-5")}>
 				<ResponsiveMasonry columnsCountBreakPoints={masonryBreakpoints} className={classNames(styles.contributionGallery)}>
 					<Masonry gutter={"1.5rem"}>
-						{data.images.map((d, i) => (
-							<img key={"img-" + i} style={{ width: "100%" }} src={basePath + "/images/contributions-media/" + data.pc_id + "/" + d} />
+						{images.map((d, i) => (
+							// <img key={"img-" + i} style={{ width: "100%" }} src={d.src} />
+							<ExportedImage
+								key={"img-" + i}
+								src={basePath + "/images/contributions-media/" + data.pc_id + "/" + d.src.split("/").slice(-1)[0]}
+								alt={"Image of " + data.title}
+								layout="responsive"
+								width={d.width}
+								height={d.height}
+								placeholder="blur"
+							/>
 						))}
 					</Masonry>
 				</ResponsiveMasonry>
 			</Col>
 			<Col sm={{ span: 12, offset: 0 }} md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 8, offset: 2 }}>
-				<p className={classNames("mb-5")} style={{ whiteSpace: "pre-line" }}>{data.description.split("\n").join("\n")}</p>
+				<p className={classNames("mb-5")} style={{ whiteSpace: "pre-line" }}>
+					{data.description.split("\n").join("\n")}
+				</p>
 			</Col>
 			{/* {data.video && (
 				<Col sm={{ span: 12, offset: 0 }} md={{ span: 10, offset: 1 }} className={classNames("mb-5")}>
@@ -56,14 +102,24 @@ export default function Contribution({ data }) {
 				<Col key={"auth" + i} sm={{ span: 12, offset: 0 }} md={{ span: 10, offset: (i % 3) + 1 }}>
 					<Row className={classNames("d-flex", "align-items-center", "mb-5")}>
 						<Col sm={2}>
-							{d.image && <img className={classNames("w-100")} src={basePath + "/images/contributions-media/" + data.pc_id + "/" + d.image} />}
+							{(d.image && authorsImages.length>0) && (
+								// <img className={classNames("w-100")} src={basePath + "/images/contributions-media/" + data.pc_id + "/" + d.image} />
+								<ExportedImage
+									src={basePath + "/images/contributions-media/" + data.pc_id + "/" + authorsImages[i].src.split("/").slice(-1)[0]}
+									alt={"Image of " + data.title}
+									layout="responsive"
+									width={authorsImages[i].width}
+									height={authorsImages[i].height}
+									placeholder="blur"
+								/>
+							)}
 						</Col>
 						<Col sm={7}>
 							<p className={classNames("mt-0", "mb-0", "fw-semibold")}>
 								{d.name} {d.surname}{" "}
 								{d.website && (
 									<a className={classNames()} href={d.website}>
-										{d.website.replace("https://", "").replace("http://", "").replace("www.", "").replace(/[\/$]/,"")}
+										{d.website.replace("https://", "").replace("http://", "").replace("www.", "").replace(/[\/$]/, "")}
 									</a>
 								)}
 							</p>
@@ -94,16 +150,4 @@ export async function getStaticProps({ params }) {
 			data,
 		},
 	};
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-function getRandomItem(arr) {
-	// get random index value
-	const randomIndex = Math.floor(Math.random() * arr.length);
-
-	// get random item
-	const item = arr[randomIndex];
-
-	return item;
 }
